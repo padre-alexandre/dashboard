b1075c84d24fed03e4528ea8f8c509faed8c4739
carol-zilker <carolinezilker@gmail.com>
2024-03-28 19:05:48 -0300

    Primeiro Commit

--- mentoria.py
+++ mentoria.py
@@ -437,7 +437,7 @@ def mostrar_mentoria(nome, permissao):
                     aux2 = aux[col].mean()
                     st.write(aux2)
                     #st.write(aux)
-                    #medias[col][0] = aux
+                    medias[col][0] = aux2
                     #medias.loc[col] = mentoria_presenca[col][mentoria_presenca[col] > 0].mean()
                 #else:
                     #medias.loc[col] = mentoria_presenca[col].mean()
