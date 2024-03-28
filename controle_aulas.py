fbad5bbce2b2704844d2675f81b1637b826a5d97
carol-zilker <carolinezilker@gmail.com>
2024-03-28 17:13:16 -0300

    Primeiro Commit

--- controle_aulas.py
+++ controle_aulas.py
@@ -245,6 +245,13 @@ def graficos_semana(valor_por_semana, aulas_por_professor):
 
 
             st.dataframe(aulas_por_professor)
+
+            numeric_cols = aulas_por_professor.select_dtypes(include='number')
+
+            # Agrupar por 'Área' e calcular a média das colunas numéricas
+            grouped_df = numeric_cols.groupby(aulas_por_professor['Área']).mean().reset_index()
+
+            st.dataframe(grouped_df)
             aulas_por_professor2 = aulas_por_professor.groupby('Área').mean().reset_index()
 
             df_plot = aulas_por_professor2.copy()
